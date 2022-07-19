23922ca21efa5c337b3ed3169b692d599dbab8cc
myzerx <cassola.delemos@hotmail.com>
2022-07-19 19:10:30 -0300

    chore: add Python + SAP GUI script

---
+++ config.py
@@ -0,0 +1,13 @@
+import os 
+
+#configurar de acordo com o ambiente que vai ser conectado
+SAP_CONNECT = 'teste'
+SAP_SID = 'xxx'
+SAP_MANDANT = '00'
+SAP_USER = 'xxxxx'
+SAP_PASS = 'xxxxx'
+SAP_EXE = "C:\Program Files (x86)\SAP\FrontEnd\SAPgui\saplogon.exe"
+
+ICONIFY = False
+
+SCR_ERR = os.path.join('path', 'err_screen.png')
---
+++ main.py
@@ -0,0 +1,41 @@
+import win32com.client
+import subprocess
+import time
+import config
+
+def getSAPTransaction(self):
+        session = self.connection.Children(0)
+        #acessando a transação SE16N
+        session.findById("wnd[0]/tbar[0]/okcd").text = "/nse16n"
+        session.findById("wnd[0]").sendVKey(0)
+        #Tabelas VBAP
+        session.findById("wnd[0]/usr/ctxtGD-TAB").text = "VBAP"
+        session.findById("wnd[0]").sendVKey(8)
+
+
+class SapGui(object):
+    def __init__(self):     
+        try:
+            #verifica se o SAP GUI já está aberto
+            sapgui = win32com.client.GetObject('SAPGUI')           
+        except:
+            #se não estiver aberto, abrirá via .exe
+                self.path = config.SAP_EXE
+                varteste = subprocess.Popen(self.path)
+                time.sleep(5)               
+        finally:
+            SapGuiAuto = win32com.client.GetObject("SAPGUI")
+        application = SapGuiAuto.GetScriptingEngine
+        #faz conexão com a connection do SAP_CONNECT
+        self.connection = application.OpenConnection(config.SAP_CONNECT, True)
+        time.sleep(3)
+        session = self.connection.Children(0)
+        
+        session.findById("wnd[0]/usr/txtRSYST-BNAME").text = config.SAP_USER
+        session.findById("wnd[0]/usr/pwdRSYST-BCODE").text = config.SAP_PASS
+        session.findById("wnd[0]").sendVKey(0)
+        getSAPTransaction(self)
+       
+    
+if __name__ == '__main__':
+ Object  =  SapGui()
